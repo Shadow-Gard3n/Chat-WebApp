0569c0faaaa9309311e238527c5e3620cb0ff4a4
Aryan Gahlot <aryangahlot194750@gmail.com>
2025-06-20 23:28:23 +0530

    Sent connectedUser data from backend to frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,58 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { fetchWithAuth } from "../utils/Api";
+import { jwtDecode } from "jwt-decode";
+
 function Home() {
+  const { accessToken, setAccessToken } = useAuth();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    const decoded = jwtDecode(accessToken);
+    const username = decoded?.username;
+
+    const fetchUsers = async () => {
+      try {
+        const res = await fetchWithAuth(
+          `http://localhost:3500/api/user/${username}/friends`,
+          { method: "GET" },
+          accessToken,
+          setAccessToken
+        );
+        const data = await res.json();
+        console.log(data);
+        console.log(data.friends);
+        setUsers(data.friends || []);
+      } catch (err) {
+        console.error("Error fetching users:", err.message);
+      }
+    };
+
+    fetchUsers();
+  }, [accessToken, setAccessToken]);
+
+  const username = accessToken ? jwtDecode(accessToken)?.username : "";
+  console.log(username);
   return (
-    <div>
-      <h1>Hello</h1>
-      <h2>hi</h2>
+    <div className="p-6 min-h-screen bg-gray-900 text-white">
+      <h1 className="text-3xl font-semibold mb-6 text-center">
+        Welcome, {username}
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {users.map((user, index) => (
+          <button
+            key={index}
+            className="bg-gray-800 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded-lg shadow-md transition duration-300"
+          >
+            {user}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
+
 export default Home;
--- controller/connect/connectedUserController.js
+++ controller/connect/connectedUserController.js
@@ -3,13 +3,11 @@ const ConnectedUsers = require('./../../model/connectedUser');
 const connectedUserHandler = async(req, res)=>{
     try {
       const { username } = req.params;
-
       if (!username) return res.status(400).json({ error: "Username parameter is required" });
 
       // find the user 
       const userConnections = await ConnectedUsers.findOne({ username });
       if (!userConnections) return res.status(404).json({ message: "User not found or no connections" });
-
       return res.status(200).json({ friends: userConnections.connectedUsers });
       
     } catch (err) {
