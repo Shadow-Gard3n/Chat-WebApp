d03b98702459b5a34fd1864007528138f8626014
Aryan Gahlot <aryangahlot194750@gmail.com>
2025-06-24 15:16:03 +0530

    Production changes+

--- client/src/utils/endpoint.js
+++ client/src/utils/endpoint.js
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3500"
-    : "https://chattrix-gcb9.onrender.com";
+export const BASE_URL ="https://chattrix-gcb9.onrender.com";
